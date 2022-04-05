fbaee0093ab9483641be848c6be5cd98de7a500c
Havis02 <holly2.havis@live.uwe.ac.uk>
2022-04-05 16:23:20 +0100

    added butterfly - no music

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,35 @@
+let yoff = 0
+
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight);
 }
 
 function draw() {
-  // put drawing code here
+  background(255);
+  translate(width/2, height/2);
+  stroke(0);
+  fill(191,213,232);
+  strokeWeight(2);
+
+  let da = PI / 100;
+  let dx = 0.05;
+
+  let xoff = 0;
+  beginShape();
+  for (let a = 0; a <=TWO_PI; a += da) {
+    let n = noise(xoff, yoff);
+    let r = sin(2 * a)* map(n, 0, 0.5, 120, 300);
+    let x = r* cos(a);
+    let y = r* sin(a);
+    if (a < PI) {
+      xoff += dx;
+    } else {
+      xoff -= dx;
+    }
+    vertex(x,y);
+  }
+  endShape();
+  yoff += 0.01;
+
+  
 }
